d72dec8c81c987825a5135f0c648f96b43e60c2a
Yang Zhang <yang_zhang@iapcm.ac.cn>
2019-05-29 02:07:40 +0800

    Fix uploading and test

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ for n in conda.recipe/*; do
   conda build $n --output-folder ${HOME}/assets
 done
 
-for n in $(find ${HOME}/assets *.tar.bz2); do
+for n in $(find ${HOME}/assets -name "*.tar.bz2"); do
   anaconda -t $CONDA_UPLOAD_TOKEN upload -u programfan $n --force
 done
 
