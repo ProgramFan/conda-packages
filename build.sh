b348c39c5bdbc3bf92641d47ca0f62ef7ee06e9d
Yang Zhang <yang_zhang@iapcm.ac.cn>
2019-05-29 01:15:58 +0800

    Enable conda-build

--- build.sh
+++ build.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 #
 
+export PATH=${HOME}/miniconda/bin:$PATH
+
+rm -rf ${HOME}/assets
+mkdir -p ${HOME}/assets
 for n in conda.recipe/*; do
-  echo $n
+  echo "====================== Building $n =========================="
+  conda build $n --output-folder ${HOME}/assets
 done
+
 exit 0
