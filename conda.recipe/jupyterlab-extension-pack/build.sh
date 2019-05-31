1588ae131b9cb3184d58455e0c785a46aba40797
Yang Zhang <yang_zhang@iapcm.ac.cn>
2019-05-31 12:19:53 +0800

    Try to build jupyterlab-extension-pack for offline use

--- conda.recipe/jupyterlab-extension-pack/build.sh
+++ conda.recipe/jupyterlab-extension-pack/build.sh
@@ -6,16 +6,16 @@ export NODE_OPTIONS=--max-old-space-size=4096
 export JUPYTER="$PREFIX/bin/jupyter"
 
 # Extensions to install
-${JUPYTER} labextension install @jupyterlab/toc@0.6 --no-build
-${JUPYTER} labextension install @jupyterlab/git@0.5 --no-build
-${JUPYTER} labextension install @jupyterlab/plotly-extension@0.18 --no-build
-${JUPYTER} labextension install @jupyterlab/vega4-extension@0.18 --no-build
-${JUPYTER} labextension install @jupyterlab/vega5-extension@1.0.0-alpha.4 --no-build
-${JUPYTER} labextension install @jupyter-widgets/jupyterlab-manager@0.38 --no-build
-${JUPYTER} labextension install jupyterlab-dash@0.1.0-alpha.2 --no-build
-${JUPYTER} labextension install jupyterlab-chart-editor@1.1 --no-build
-${JUPYTER} labextension install plotlywidget@0.10 --no-build
-${JUPYTER} labextension install plotlylab-light-theme --no-build
-${JUPYTER} labextension install @mflevine/jupyterlab_html --no-build
+${JUPYTER} labextension install @jupyterlab/toc@0.6
+${JUPYTER} labextension install @jupyterlab/git@0.5
+${JUPYTER} labextension install @jupyterlab/plotly-extension@0.18
+${JUPYTER} labextension install @jupyterlab/vega4-extension@0.18
+${JUPYTER} labextension install @jupyterlab/vega5-extension@1.0.0-alpha.4
+${JUPYTER} labextension install @jupyter-widgets/jupyterlab-manager@0.38
+${JUPYTER} labextension install jupyterlab-dash@0.1.0-alpha.2
+${JUPYTER} labextension install jupyterlab-chart-editor@1.1
+${JUPYTER} labextension install plotlywidget@0.10
+${JUPYTER} labextension install plotlylab-light-theme
+${JUPYTER} labextension install @mflevine/jupyterlab_html
 
 unset NODE_OPTIONS
