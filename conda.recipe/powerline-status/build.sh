330de2bed84e4e43ca5bbbc89b5ac3959879876a
Zhang YANG <zyangmath@gmail.com>
2019-06-24 12:34:35 +0800

    Update build.sh

--- conda.recipe/powerline-status/build.sh
+++ conda.recipe/powerline-status/build.sh
@@ -4,4 +4,4 @@
 python ./setup.py build
 python ./setup.py install
 
-${CC:gcc} -O3 -g -o ${PREFIX}/bin/powerline ${SRC_DIR}/client/powerline.c -lrt 
+${CC:-gcc} -O3 -g -o ${PREFIX}/bin/powerline ${SRC_DIR}/client/powerline.c -lrt 
