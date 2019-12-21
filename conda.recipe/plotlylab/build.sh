4289bcdc9fb4d9dee14ef1cf138fb7a034818443
Yang Zhang <yang_zhang@iapcm.ac.cn>
2019-12-21 23:56:53 +0800

    Fix plotlylab upgrade

--- conda.recipe/plotlylab/build.sh
+++ conda.recipe/plotlylab/build.sh
@@ -9,16 +9,14 @@ export NODE_OPTIONS=--max-old-space-size=4096
 # Set JUPYTERLAB_DIR before installing to isolate our extensions
 export JUPYTERLAB_DIR=$PREFIX/share/jupyter/plotlylab
 
+ARGS="--no-build --log-level=INFO"
 # Extensions to install
-jupyter labextension install @jupyterlab/toc --no-build
-jupyter labextension install @jupyterlab/git --no-build
-jupyter labextension install @jupyterlab/plotly-extension --no-build
-jupyter labextension install @jupyter-widgets/jupyterlab-manager --no-build
-jupyter labextension install jupyterlab-dash --no-build
-jupyter labextension install jupyterlab-chart-editor --no-build
-jupyter labextension install plotlywidget --no-build
-jupyter labextension install plotlylab-light-theme --no-build
-jupyter labextension install @mflevine/jupyterlab_html --no-build
+jupyter labextension install @jupyterlab/toc@1.0.1 ${ARGS} 
+jupyter labextension install @jupyterlab/git@0.9.0 ${ARGS}
+jupyter labextension install @jupyter-widgets/jupyterlab-manager@1.1.0 ${ARGS}
+jupyter labextension install jupyterlab-dash@0.1.0-alpha.3 ${ARGS}
+jupyter labextension install @jupyterlab/htmlviewer ${ARGS}
+jupyter labextension install @jupyterlab/htmlviewer-extension ${ARGS}
 
 jupyter lab build --name='PlotlyLab'
 jupyter lab clean
