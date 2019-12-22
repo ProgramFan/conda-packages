d4b7e4d7f9b58a9cf19ab5019e9c0ce5fec96b1b
Yang Zhang <yang_zhang@iapcm.ac.cn>
2019-12-22 17:34:43 +0800

    Fix plolylab

--- conda.recipe/plotlylab/build.sh
+++ conda.recipe/plotlylab/build.sh
@@ -11,12 +11,10 @@ export JUPYTERLAB_DIR=$PREFIX/share/jupyter/plotlylab
 
 ARGS="--no-build --log-level=INFO"
 # Extensions to install
-jupyter labextension install @jupyterlab/toc@1.0.1 ${ARGS} 
-jupyter labextension install @jupyterlab/git@0.9.0 ${ARGS}
+jupyter labextension install @jupyterlab/toc ${ARGS} 
+jupyter labextension install @jupyterlab/git ${ARGS}
 jupyter labextension install @jupyter-widgets/jupyterlab-manager@1.1.0 ${ARGS}
 jupyter labextension install jupyterlab-dash@0.1.0-alpha.3 ${ARGS}
-jupyter labextension install @jupyterlab/htmlviewer ${ARGS}
-jupyter labextension install @jupyterlab/htmlviewer-extension ${ARGS}
 
 jupyter lab build --name='PlotlyLab'
 jupyter lab clean
